5a4fa8d874848fc91c36ee1ecd8aeb9a4dc5c175
acth2 <iliane57950@gmail.com>
2025-02-19 18:55:41 +0100

    Adding assets and a block logic

--- src/main/java/fr/acth2/mod/Main.java
+++ src/main/java/fr/acth2/mod/Main.java
@@ -1,5 +1,6 @@
 package fr.acth2.mod;
 
+import fr.acth2.mod.blocks.ModBlocks;
 import fr.acth2.mod.items.ModItems;
 import fr.acth2.mod.proxy.ClientProxy;
 import fr.acth2.mod.proxy.CommonProxy;
@@ -21,6 +22,7 @@ public class Main {
     public Main() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         modEventBus.addListener(this::setup);
     }
 
--- src/main/java/fr/acth2/mod/utils/References.java
+++ src/main/java/fr/acth2/mod/utils/References.java
@@ -1,7 +1,5 @@
 package fr.acth2.mod.utils;
 
 public class References {
-    public static final String MODID = "jsp";
-    public static final String CLIENT_PROXY = "fr.acth2.mod.proxy.ClientProxy";
-    public static final String COMMON_PROXY = "fr.acth2.mod.proxy.CommonProxy";
+    public static final String MODID = "ror";
 }

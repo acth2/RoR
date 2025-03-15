bd11a57863c594746b1d39429d3f0dafd7ff2391
acth2 <iliane57950@gmail.com>
2025-03-15 23:07:06 +0100

    Adding the WaterMonsterEntity because minecraft haven't (or im dumb)

---
+++ src/main/java/fr/acth2/ror/entities/constructors/aquamarin/AquamarinEntity.java
@@ -0,0 +1,99 @@
+package fr.acth2.ror.entities.constructors.aquamarin;
+
+import fr.acth2.ror.entities.constructors.WaterMonsterEntity;
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.WaterMobEntity;
+import net.minecraft.entity.passive.fish.AbstractFishEntity;
+import net.minecraft.entity.passive.fish.AbstractGroupFishEntity;
+import net.minecraft.entity.passive.fish.SalmonEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class AquamarinEntity extends WaterMonsterEntity {
+
+    protected AquamarinEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
+        super(type, worldIn);
+    }
+
+    @Override
+    public CreatureAttribute getMobType() {
+        return CreatureAttribute.UNDEFINED;
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.addBehaviourGoals();
+    }
+
+
+    protected void addBehaviourGoals() {
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false) {
+        });
+
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomWalkingGoal(this, 1.0D) {
+        });
+
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, PlayerEntity.class, true) {
+        });
+    }
+
+    public boolean causeFallDamage(float p_225503_1_, float p_225503_2_) {
+        return false;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return null;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return super.getHurtSound(p_184601_1_);
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return super.getDeathSound();
+    }
+
+    public int getAmbientSoundInterval() {
+        return 60;
+    }
+
+    @Override
+    protected boolean isSunBurnTick() {
+        return false;
+    }
+
+    @Override
+    public void setGlowing(boolean p_184195_1_) {
+        super.setGlowing(p_184195_1_);
+    }
+
+    public static AttributeModifierMap.MutableAttribute createAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 6.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.226D)
+                .add(Attributes.ATTACK_DAMAGE, 4.0D);
+    }
+}

a80dcc479b4744de85c8396c7089f09222e5c5db
acth2 <iliane57950@gmail.com>
2026-07-08 19:54:51 +0200

    Revert "Fixing imports in 1.18.2 part 4"
    This reverts commit e78700e5

--- src/main/java/fr/acth2/ror/entities/renderer/creckon/CreckonRenderer.java
+++ src/main/java/fr/acth2/ror/entities/renderer/creckon/CreckonRenderer.java
@@ -1,13 +1,13 @@
 package fr.acth2.ror.entities.renderer.creckon;
 
-import fr.acth2.ror.entities.entity.corrupted.EntityCorrupted;
-import fr.acth2.ror.entities.models.corrupted.CorruptedModel;
+import fr.acth2.ror.entities.entity.creckon.EntityCreckon;
+import fr.acth2.ror.entities.models.creckon.CreckonModel;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class CreckonRenderer extends GeoEntityRenderer<EntityCorrupted> {
+public class CreckonRenderer extends GeoEntityRenderer<EntityCreckon> {
     public CreckonRenderer(EntityRendererManager manager) {
-        super(manager, new CorruptedModel());
+        super(manager, new CreckonModel());
         this.shadowRadius = 1.0f;
     }
 }
